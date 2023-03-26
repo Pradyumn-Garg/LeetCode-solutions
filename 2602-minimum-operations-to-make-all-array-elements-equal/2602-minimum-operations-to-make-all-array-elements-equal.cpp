89e37e386cd8a8da2ddc64faaaa4def14e4943c0
Pradyumn Garg <86672147+Pradyumn-Garg@users.noreply.github.com>
2023-03-26 19:55:42 +0530

    Time: 280 ms (50.00%), Space: 81 MB (75.00%) - LeetHub

---
+++ 2602-minimum-operations-to-make-all-array-elements-equal/2602-minimum-operations-to-make-all-array-elements-equal.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    // explaination - https://leetcode.com/problems/minimum-operations-to-make-all-array-elements-equal/discuss/3341928/C%2B%2B-Java-Python3-Prefix-Sums-%2B-Binary-Search
+    vector<long long> minOperations(vector<int>& nums, vector<int>& queries) {
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    vector<long long> ans, prefix(n + 1);
+    for (int i = 0; i < n; i++)
+        prefix[i + 1] = prefix[i] + (long long) nums[i];
+    for (int x : queries) {
+        int i = lower_bound(nums.begin(), nums.end(), x) - nums.begin();
+        ans.push_back(1LL * x * (2 * i - n) + prefix[n] - 2 * prefix[i]);
+    }
+    return ans;
+}
+};
