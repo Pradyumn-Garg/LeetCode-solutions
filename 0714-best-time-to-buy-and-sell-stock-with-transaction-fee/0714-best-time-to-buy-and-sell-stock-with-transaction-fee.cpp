523335c2e9e9b0ab4708e7e32ed5b602a67e067e
Pradyumn Garg <86672147+Pradyumn-Garg@users.noreply.github.com>
2023-06-22 18:54:49 +0530

    Time: 117 ms (84.77%), Space: 54.9 MB (98.47%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,45 +1,28 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
-        int ans=0,curr_min=INT_MAX,n=prices.size(),curr_max=INT_MIN,flag=0;
+        int ans=0,n=prices.size(),flag=0,min=INT_MAX,max=INT_MIN;
         for(int i=0;i<n;i++){
             if(flag==1){
-                if(curr_max-prices[i]>fee){
-                    ans+=curr_max-curr_min-fee;
-                    curr_min=prices[i];
-                    curr_max=INT_MIN;
+                if(max-prices[i]>fee){
+                    ans+=max-min-fee;
+                    min=prices[i];
+                    max=INT_MIN;
                     flag=0;
                     continue;
                 }
             }
-            if(prices[i]<curr_min){
-                curr_min=prices[i];
+            if(prices[i]<min){
+                min=prices[i];
             }
-            else if((prices[i]-curr_min>fee)&&(i==n-1||prices[i]>prices[i+1])&&prices[i]>curr_max){
-                curr_max=prices[i];
+            else if((prices[i]-min>fee)&&(i==n-1||prices[i]>prices[i+1])&&prices[i]>max){
+                max=prices[i];
                 flag=1;
             }
         }
-        
         if(flag==1){
-            ans+=curr_max-fee-curr_min;
+            ans+=max-fee-min;
         }
-        // else if(i==n-2&&prices[i]-curr>fee){
-        //     if(prices[n-1]>prices[n-2]){
-        //         ans+=prices[n-1]-curr-fee;
-        //     }
-        //     else{
-        //         ans+=prices[i]-curr-fee;
-        //     }
-        //     curr=INT_MAX;
-        // }
-
-        
-        // if(curr!=INT_MAX){
-        //     if(prices[n-1]-curr>fee){
-        //         ans+=prices[n-1]-fee-curr;
-        //     }
-        // }
         return ans;
     }
 };
