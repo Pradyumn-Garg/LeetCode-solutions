9ecc6fcf1ded6752315dff8b3b285b04f26fa56d
Pradyumn Garg <86672147+Pradyumn-Garg@users.noreply.github.com>
2023-06-25 17:45:34 +0530

    Time: 187 ms (79.01%), Space: 12.2 MB (16.02%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
+    int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
+        vector <vector<int>> dp(fuel+1,vector<int>(locations.size()+1,-1));
+        return rec(locations, start, finish, fuel, dp);
+    }
     int rec(vector <int> &locations,int curr, int finish, int fuel, vector<vector<int>> &dp){
-        if(fuel<0){
+        if(fuel==0){
+            if(curr==finish){
+                return 1;
+            }
             return 0;
         }
-        if(dp[curr][fuel]!=-1){
-            return dp[curr][fuel];
+        if(dp[fuel][curr]!=-1){
+            return dp[fuel][curr];
         }
-        
-        int sum = curr == finish ? 1 : 0;
-
+        int sum=0;
         for(int i=0;i<locations.size();i++){
-            if(i!=curr){
+            if(i==curr){
+                continue; 
+            }
+            if(fuel-abs(locations[i]-locations[curr])>=0){
                 sum=(sum+rec(locations,i,finish,fuel-abs(locations[i]-locations[curr]),dp))%1000000007;
             }
         }
-        return dp[curr][fuel]=sum;
-    }
-
-    int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
-        int n=locations.size();
-        vector <vector<int>> dp(n,vector<int>(fuel+1,-1));
-        return rec(locations, start, finish, fuel, dp);
+        if(curr==finish){
+            sum+=1;
+        }
+        return dp[fuel][curr]=sum;
     }
 };
