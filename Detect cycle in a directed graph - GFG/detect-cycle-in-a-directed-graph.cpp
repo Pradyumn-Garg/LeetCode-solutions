2963d411922683e748ff19454506eda80200990c
Pradyumn Garg <86672147+Pradyumn-Garg@users.noreply.github.com>
2023-07-19 01:52:34 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -5,72 +5,37 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    bool isCyclicUtil(int v, vector<bool> &visited, vector<bool> &recStack,
-                  vector<int> adj[]) {
-        // if the current vertex is not visited.
-        if (visited[v] == false) {
-            // marking the current node as visited and part of recursion stack.
-            visited[v] = true;
-            recStack[v] = true;
-
-            // calling function recursively for all the vertices
-            // adjacent to this vertex.
-            for (int i = 0; i < (int)adj[v].size(); ++i) {
-                if (!visited[adj[v][i]] &&
-                    isCyclicUtil(adj[v][i], visited, recStack, adj))
-                    return true;
-                else if (recStack[adj[v][i]])
-                    return true;
-            }
-        }
-        // removing the vertex from recursion stack.
-        recStack[v] = false;
-        return false;
-    }
-
     // Function to detect cycle in a directed graph.
     bool isCyclic(int V, vector<int> adj[]) {
-        // marking all vertices as not visited and not a part of recursion stack
-        vector<bool> visited(V, false);
-        vector<bool> recStack(V, false);
-
-        // calling the recursive helper function to detect cycle in
-        // different DFS trees.
-        for (int i = 0; i < V; i++)
-            if (isCyclicUtil(i, visited, recStack, adj)) return true;
-
+        vector<int> v2(V,0);
+        vector<int> v(V,0);
+        for(int i=0;i<V;i++){
+            if(v2[i]==0){
+                v2[i]=1;
+                if(dfs(adj,v2,v,i)){
+                    return true;
+                }
+            }
+        }
         return false;
     }
-
-    // bool isCyclic(int V, vector<int> adj[]) {
-    //     vector<int> v2(V,0);
-    //     vector<int> v(V,0);
-    //     for(int i=0;i<V;i++){
-    //         if(v2[i]==0){
-    //             v2[i]=1;
-    //             if(dfs(adj,v2,v,i)){
-    //                 return true;
-    //             }
-    //         }
-    //     }
-    //     return false;
-    // }
     
-    // bool dfs(vector <int> adj[],vector<int> &v2,vector<int> v,int curr){
-    //     v[curr]=1;
-    //     for(auto i:adj[curr]){
-    //         if(v[i]==0&&v2[i]==0){
-    //             v2[i]=1;
-    //             if(dfs(adj,v2,v,i)){
-    //                 return true;
-    //             }
-    //         }
-    //         else if(v[i]==1){
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
+    bool dfs(vector <int> adj[],vector<int> &v2,vector<int> &v,int curr){
+        v[curr]=1;
+        for(auto i:adj[curr]){
+            if(v[i]==0&&v2[i]==0){
+                v2[i]=1;
+                if(dfs(adj,v2,v,i)){
+                    return true;
+                }
+            }
+            else if(v[i]==1){
+                return true;
+            }
+        }
+        v[curr]=0;
+        return false;
+    }
 };
 
 //{ Driver Code Starts.
